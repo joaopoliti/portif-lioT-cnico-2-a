dbea1cf9bd7d8801e4e41a202ace14d3987b8343
joaopoliti <82882238+joaopoliti@users.noreply.github.com>
2021-06-21 18:49:49 -0300

    script que fala a media e o produto + caro

--- exercícios-shellScript/media.sh
+++ exercícios-shellScript/media.sh
@@ -1,1 +1,19 @@
-
+#!/bin/bash
+#script que tira a media dos produtos 
+#Nome: João Paulo
+i=1
+soma=0
+while [ $i -le 15 ]
+do 
+    echo " Escreva o preço do $i° produto"
+    read: valor 
+    if [ $valor -lt $maior ]
+    then 
+        maior=$valor
+     fi
+     soma=$(( $soma + $valor ))
+     i=$(( $i + 1 ))
+done
+soma=$( bc <<< "scale=4; $soma/15" )
+echo " A media dos valores é $soma "
+echo " O produto mais caro é $maior" 
